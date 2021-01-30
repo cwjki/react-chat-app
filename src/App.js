65176de7e8d99a0aa8a025225376d0f63c17fdbf
cwjki <cwjki97@gmail.com>
2021-01-30 00:18:56 -0500

    MyMessage ChatFeed components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine 
+      height="100vh"
+      projectID="9a579d0e-3cde-444a-bda9-3fb2ebe8dd19"
+      userName="cwjki97"
+      userSecret="123456"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+}
+
+export default App;
