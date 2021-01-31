7d0797cf27cc8a280e0ce06178e566036370b7eb
cwjki <cwjki97@gmail.com>
2021-01-31 00:51:23 -0500

    messageForm uploading img, still have some bug

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
   const [value, setValue] = useState('');
@@ -10,11 +11,17 @@ const MessageForm = (props) => {
     const text = value.trim();
 
     if (text.length > 0) sendMessage(creds, chatId, { text });
+  
+    setValue('');
   }
 
   const handleChange = (e) => {
     setValue(e.target.value);
     isTyping(props, chatId);
+  }
+
+  const handleUpload = (e) => {
+    sendMessage(creds, chatId, { files: e.target.files, text: '' })
   }
 
   return (
@@ -26,6 +33,21 @@ const MessageForm = (props) => {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input 
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: 'none' }}
+        onChange={handleUpload}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
     </form>
   );
 }
